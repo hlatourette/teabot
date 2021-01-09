3539e07c30a6d322e0834c661fcab00c74d4f8de
Hunter LaTourette <hlatourette@gmail.com>
2021-01-09 18:59:04 -0500

    feat (status): add simple bot tea status

--- src/clients/discord_client.py
+++ src/clients/discord_client.py
@@ -2,6 +2,9 @@ import discord
 
 
 class DiscordClient(discord.Client):
+    async def on_ready(self):
+        await self.change_presence(activity = discord.Game('🍵'))
+
     async def on_message(self, message):
         # bot won't respond to itself
         if message.author == self.user:
